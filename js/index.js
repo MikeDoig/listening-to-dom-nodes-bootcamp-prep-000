03ca5dabca2a89755a8fad2ac6b5cc81fa0e5060
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 16:59:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,5 +9,11 @@ main.addEventListener('click', function(event) {
 const input = document.querySelector('input');
  
 input.addEventListener('keydown', function(e) {
+  if (e.key == "g") {
+    return e.preventDefault();
+  }
+  else {
   console.log(e.key);
+  }
 });
+
