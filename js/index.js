cd6b98037627606069ee184872e535f607aa2c39
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 17:09:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,8 +17,15 @@ input.addEventListener('keydown', function(e) {
   }
 });
 
+// let divs = document.querySelectorAll('div');
+// function bubble(e) {
+//   console.log(this.firstChild.nodeValue.trim() + 'bubbled');
+// }
+// for (let i=0;i<divs.length;i++){
+// divs[i].addEventListener('click', bubble); 
+// }
 let divs = document.querySelectorAll('div');
-function bubble(e) {
+function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + 'bubbled');
 }
 for (let i=0;i<divs.length;i++){
